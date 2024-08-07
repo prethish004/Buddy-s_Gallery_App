dda472996fe442d0bbdeb6d3580d7aacaf82c269
prethish004 <106960902+prethish004@users.noreply.github.com>
2024-08-07 22:38:54 +0530

    App.tsx

--- Gallary/image_Gallery/src/App.tsx
+++ Gallary/image_Gallery/src/App.tsx
@@ -10,7 +10,6 @@ function App() {
     <AuthProvider>
     <Routes>
       <Route path="/" element={<PublicRoute><Signup/></PublicRoute>}/>
-      <Route path="/h" element={<PrivateRoute><Home/></PrivateRoute>}/>
       <Route path="/*" element={<PrivateRoute><Home/></PrivateRoute>}/>
     </Routes>
     </AuthProvider>
