5ba19b95198c4baf63560fefb5d6632e81883bc4
prethish004 <106960902+prethish004@users.noreply.github.com>
2024-08-09 22:15:48 +0530

    Home.tsx

--- Gallary/image_Gallery/src/pages/Home.tsx
+++ Gallary/image_Gallery/src/pages/Home.tsx
@@ -5,12 +5,7 @@ import UploadFrom from "../components/UploadFrom";
 const Home=()=>{
    
     return(
-        <div className='max-w-4xl mx-auto' style={{ 
-                backgroundImage: "url('https://wallpapers.com/images/featured/colorful-abstract-background-rra8u4adw1ubypzl.jpg')", 
-                backgroundSize: 'cover', 
-                backgroundPosition: 'center', 
-                minHeight: '100vh' 
-            }}>
+        <div className='max-w-4xl mx-auto'>
             <Navbar/>
             <UploadFrom/>
             <ImageGallery/>
